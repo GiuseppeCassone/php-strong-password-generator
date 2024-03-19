b5d91df325a4ba9166501fb65cfff5bfcaccd7ef
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-19 16:46:47 +0100

    milestone 1 completata

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+function generatePassword($length) {
+    $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=~!@#$%^&*()_+,./<>?;:[]{}\|';
+    return substr(str_shuffle($chars), 0, $length);
+}
+
+$newPassword = '';
+
+if(isset($_GET['passwordLength'])) {
+    $passwordLength = $_GET['passwordLength'];
+    $newPassword = generatePassword($passwordLength);
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,10 +29,25 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body data-bs-theme="dark">
 
-    <h1>ciao</h1>
-    
+    <div class="container p-5">
+        <h1>PASSWORD GENERATOR</h1>
+
+        <form action="index.php" method="GET" class="mb-3">
+            <label for="passwordLength">Scegli il numero di caratteri:</label>
+            <input type="number" id="passwordLength" name="passwordLength" min="8" max="25" required>
+            <button type="submit">Genera</button>
+        </form>
+
+        <?php if (!empty($newPassword)) { ?>
+            <div class="card p-5">
+                <h2>Password generata:</h2>
+                <p><?php echo $newPassword; ?></p>
+            </div>
+        <?php } ?>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
